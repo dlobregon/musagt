c3b04ec3f290655d82b526a3b8598718aa217704
dlobregon <troyanrex@gmail.com>
2017-06-25 16:14:01 -0600

    actualización de frontend

---
+++ client/app.js
@@ -0,0 +1,40 @@
+angular.module('musagtManager',
+['ngMaterial',
+'ui.router',
+'manager.controller',
+'events.controller',
+'comments.controller',
+'manager.utils',
+])
+
+
+  //creando las vistas con sus respectivos controllers
+  .config(
+    function($stateProvider, $locationProvider,$urlRouterProvider, $mdThemingProvider) {
+
+
+      $mdThemingProvider.theme('default')
+        .primaryPalette('blue-grey')
+        .accentPalette('indigo');
+
+
+      $stateProvider
+      	.state('events',{
+          url:'/events',
+          templateUrl: 'templates/events.html',
+          controller:'eventsCtrl'
+        })
+
+        .state('comments', {
+          url:'/comments',
+          templateUrl:'templates/comments.html',
+          controller:'commentsCtrl'
+        })
+
+
+        ;
+
+        $urlRouterProvider
+          .otherwise('/events');
+
+  })
